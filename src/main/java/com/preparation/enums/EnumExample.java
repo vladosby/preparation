bdfc94447a03136051a542d84dcfd95d6900d566
S4DWCX <Vladislav_Kievskiy@rcomext.com>
2017-09-20 09:30:37 +0300

    Enum example

---
+++ src/main/java/com/preparation/enums/EnumExample.java
@@ -0,0 +1,31 @@
+package com.preparation.enums;
+
+/**
+ * Description of class EnumExample
+ *
+ * @created: 9/20/2017
+ */
+public enum EnumExample {
+    SMALL(2), MEDIUM(4), BIG(8);
+
+    private int size;
+
+    EnumExample(int size) {
+        this.size = size;
+    }
+
+    public int getSize() {
+        return size;
+    }
+}
+
+
+class Main {
+    public static void main(String[] args) {
+        System.out.println(EnumExample.BIG);
+        System.out.println(EnumExample.BIG.getSize());
+        System.out.println(EnumExample.BIG.name());
+        System.out.println(EnumExample.BIG.ordinal());
+        System.out.println(EnumExample.BIG.);
+    }
+}
